1def6744abce6f77c6e843bfbc3b3e14133cb84f
Roldan549 <151262090+Roldan549@users.noreply.github.com>
2023-11-17 20:34:20 -0300

    Add files via upload

---
+++ TrabajoRoldan/Js/main.js
@@ -0,0 +1,42 @@
+let miNumero = 0;
+
+function generarNumero() {
+    miNumero = Math.floor(Math.random() * 100) + 1;
+    document.getElementById("myInput").value = miNumero;
+}
+
+function agregarANumeroLista() {
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(miNumero));
+    document.getElementById("myList").appendChild(li);
+}
+function sumarNumeros() {
+    let numeros = document.getElementById("myList").getElementsByTagName("li");
+    let suma = 0;
+    for (let i = 0; i < numeros.length; i++) {
+        suma += parseInt(numeros[i].innerText);
+    }
+    document.getElementById("resultado").innerText = "La suma es: " + suma;
+}
+
+function calcularPromedio() {
+    let numeros = document.getElementById("myList").getElementsByTagName("li");
+    let suma = 0;
+    for (let i = 0; i < numeros.length; i++) {
+        suma += parseInt(numeros[i].innerText);
+    }
+    let promedio = suma / numeros.length;
+    document.getElementById("resultadoPromedio").innerText = "El promedio es: " + promedio.toFixed(2);
+}
+
+function buscarMaximo() {
+    let numeros = [];
+    let celdas = document.getElementById("tablaNumeros").getElementsByTagName("td");
+
+    for (let i = 0; i < celdas.length; i++) {
+        numeros.push(parseInt(celdas[i].innerText));
+    }
+
+    let maximo = Math.max(...numeros);
+    alert("El número máximo es: " + maximo);
+}
